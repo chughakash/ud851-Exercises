5f1e5395b736276fccf110962739ffff45c870c4
Akash Chugh <akash.chugh@asu.edu>
2018-01-27 23:06:38 -0700

    Exercise 03 and Exercise 07

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.datafrominternet.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -31,6 +34,7 @@ public class NetworkUtils {
 
     final static String PARAM_QUERY = "q";
 
+    public static URL url;
     /*
      * The sort field. One of stars, forks, or updated.
      * Default: results are sorted by best match if no field is specified.
@@ -45,8 +49,14 @@ public class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
-        return null;
+        Uri buildUri = Uri.parse(GITHUB_BASE_URL).buildUpon().
+                appendQueryParameter(PARAM_QUERY,githubSearchQuery).appendQueryParameter(PARAM_SORT,sortBy).build();
+        url = null;
+
+        try{
+            url = new URL(buildUri.toString());
+        }catch(Exception ex){ex.printStackTrace();}
+        return url;
     }
 
     /**
